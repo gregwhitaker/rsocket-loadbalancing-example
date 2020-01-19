7c54e21714a7c64c2a1e27fedcc5f4b3049028f3
gregwhitaker <gwhitake@gmail.com>
2020-01-18 16:41:03 -0800

    Doing work

--- hello-client/src/main/java/example/hello/client/HelloClient.java
+++ hello-client/src/main/java/example/hello/client/HelloClient.java
@@ -40,7 +40,7 @@ public class HelloClient {
 
         // Create a load balancer
         LoadBalancedRSocketMono loadBalancer = LoadBalancedRSocketMono
-                .create(Flux.just(rSocketSuppliers));
+                .create(Flux.just(rSocketSuppliers).repeat());
 
         CountDownLatch latch = new CountDownLatch(10);
 
